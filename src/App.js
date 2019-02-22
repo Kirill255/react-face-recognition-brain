90bbadf325b26caec0c312039b0efe8a9b0fa5a1
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-02-22 23:49:13 +0300

    Rank comp

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
+import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 
 import "./App.css";
@@ -12,6 +13,7 @@ class App extends Component {
       <div className="App">
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/*<FaceRecognition /> */}
       </div>
