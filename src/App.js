2d61be2c90ce7af4ba1e54fcedc82257818e626a
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-02-23 02:03:56 +0300

    FaceRecognition comp & image recognition API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 
 import "./App.css";
+
+const app = new Clarifai.App({
+  apiKey: "9446ba7cd5824fa89a6482bbc6f7c80d"
+});
 
 // https://vincentgarreau.com/particles.js/
 const particlesOptions = {
@@ -32,13 +38,22 @@ class App extends Component {
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
     this.setState({ input: event.target.value });
   };
 
   onButtonSubmit = () => {
-    console.log("click");
     this.setState({ imageUrl: this.state.input });
+
+    // https://clarifai.com/models/face-detection-image-recognition-model-a403429f2ddf4b49b307e318f00e528b-detection
+    // https://github.com/Clarifai/clarifai-javascript/blob/master/src/index.js
+
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) => {
+        console.log(response);
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      })
+      .catch((err) => console.log(err));
   };
 
   render() {
@@ -49,7 +64,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        {/*<FaceRecognition /> */}
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
