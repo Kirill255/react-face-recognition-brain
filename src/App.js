8cf5731583879456bc6efbf1808c6da5c4294099
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-02-23 00:14:21 +0300

    react-particles-js comp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Particles from "react-particles-js";
 
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
@@ -7,10 +8,24 @@ import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 
 import "./App.css";
 
+// https://vincentgarreau.com/particles.js/
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 130,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
         <Logo />
         <Rank />
